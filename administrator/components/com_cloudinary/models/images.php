88c000dd6115038fa167c886a11c4f657c411ed1
Tom Janssens <tom@janssens.me>
2012-11-03 14:03:44 +0100

    Added Images Model

---
+++ administrator/components/com_cloudinary/models/images.php
@@ -0,0 +1,28 @@
+<?php
+class ComCloudinaryModelImages extends ComDefaultModelDefault
+{
+	public function __construct(KConfig $config)
+	{
+		parent::__construct($config);
+		
+		$this->_state
+			->insert('table', 'cmd')
+			->insert('row', 'int')
+			->insert('dimensions', 'string', 'w_50');
+	}
+	
+	protected function _buildQueryWhere(KDatabaseQuery $query)
+	{
+		parent::_buildQueryWhere($query);
+		
+		if(!$this->_state->isUnique()) {
+			if($this->_state->table) {
+				$query->where('tbl.table','=', $this->_state->table);
+			}
+
+			if($this->_state->row) {
+				$query->where('tbl.row', '=',  $this->_state->row);
+			}
+		}
+	}
+}
