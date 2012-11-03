908140a7b68a2cb66eb23c36e03c444d7e7f0bc7
Tom Janssens <tom@janssens.me>
2012-11-03 13:21:11 +0100

    Cleanup

--- administrator/components/com_cloudinary/databases/rows/image.php
+++ administrator/components/com_cloudinary/databases/rows/image.php
@@ -14,15 +14,14 @@ class ComCloudinaryDatabaseRowImage extends KDatabaseRowDefault
 		require JPATH_LIBRARIES.DS.'cloudinary/cloudinary.php';
 		require JPATH_LIBRARIES.DS.'cloudinary/uploader.php';
 		
-		$this->file 	= $_FILES['file']['tmp_name'];
-		$this->name 	= $_FILES['file']['name'];
+		$file 	= $_FILES['file']['tmp_name'];
 		
 		$component = $this->getService('com://admin/extensions.model.components')->id('33')->getItem();
 		$params = $component->params->toArray();
 		
 		Cloudinary::config(array("cloud_name" => $params['cloud_name'], "api_key" => $params['api_key'], "api_secret" => $params['api_secret']));
 		
-		$result = Cloudinary\Uploader::upload($this->file, array("tags"=> ""));
+		$result = Cloudinary\Uploader::upload($file, array("tags"=> ""));
 		
 		$this->public_id = $result['public_id'];
 		
